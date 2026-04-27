a04eb7fa0367973035dff8c167e236a1ae709046
IceBearOsa <alex@cheebi.com>
2026-04-26 23:56:31 -0700

    Hour 10

---
+++ ahartman_hour10/Assets/GameCritics.cs
@@ -0,0 +1,11 @@
+/*
+    1. Game is a little easy.
+    2. Could add a little more difficulty through adding obstacles.
+    3. Maybe fun graphics or sound effects.
+    4. The part of it thats fun is the little lights in the corners, what is hard is how fast the balls can move.
+
+    What I changed:
+    1. I added a "Play Again?" button that restarts the game when you click it.
+    2. Little obstacles that make it harder to get to the goals.
+    3. Changed the shape of the bumper.
+*/
