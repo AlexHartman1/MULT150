e399bab7ce96a9ffeeaf2c8caf3df39f00995d78
IceBearOsa <alex@cheebi.com>
2026-05-16 14:32:11 -0700

    Hour 14&16

---
+++ ahartman_hour14and16/Assets/Hour 14 Package/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "Game";
+
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+	public void CampFire ()
+	{
+		SceneManager.LoadScene("ahartman_hour16");
+	}
+}
---
+++ ahartman_hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,7 @@
+/*
+1.  Added a gradient for color over lifetime from red to yellow with orange in the middle.
+2.  Changed the start lifetime to 3. Changing it so the particles last shorter so you can see the effect better.
+3.  Changed the start size of the particles to 10, making the particles blend together to look more like a campfire.
+4.  Changed rate over time to 15, making more particles appear to make the fire look fuller.
+5.  Changed simulation speed to 1.5, making the particles move faster to make the fire "burn" faster.
+*/
