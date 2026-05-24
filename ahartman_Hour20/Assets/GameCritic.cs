9b6472a7e2b4882e81c5251534f37a2a6322f5ce
IceBearOsa <alex@cheebi.com>
2026-05-24 13:36:59 -0700

    Hour 20 (A little SUS)

---
+++ ahartman_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,31 @@
+/*
+  ______ _     _ ______ 
+ / _____|_)   (_) _____)
+( (____  _     ( (____  
+ \____ \| |   | \____ \ 
+ _____) ) |___| |____) )
+(______/ \_____(______/ 
+                        
+1. Its too easy and too hard at the same time, its easy because you can just stand still and wait, but the hard part is if you try, the powerups move too fast.
+2. I would change it to have the time be a little longer, and have the powerups be moving a little slower.
+3. Making some of the graphics a little better and detailed would make it more appealing.
+4. I wouldn't exactly say that it is fun, because you are doing the same thing over and over again, which I don't find fun.
+5. The parts of the game that are tedious are doing the same thing over and over again.
+
+Changes in the obstacle:
+Shape:
+    - changed the shape to be a cylinder instead of a cube
+    - added a little cube to the front of the obstacle to make it look sus
+Texture:
+    - Cropped an image from https://www.deviantart.com/brettpostscript/art/Among-Us-Impostor-Red-1014348685 
+    - Imported said new texture saying "Imposter" and made it into a new material and applied it to the obstacle
+Changes in the powerup:
+Particle system:
+    - changed the shape back to a cone
+    - changed start speed to 5
+    - changed the start lifetime to 1
+    - changed the position to 2.5 to make the particles come from inside the sphere instead of the outside
+Shape:
+    - changed the shape to be a capsule instead of a sphere
+    - added a little cube to the front of the powerup to make it look sus
+*/
