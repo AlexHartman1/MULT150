4a3409edcd7c545ae94073b6f37f5e92433e7e73
IceBearOsa <alex@cheebi.com>
2026-05-31 12:40:36 -0700

    FINAL FINAL
    Completes the whole final!

---
+++ ahartman_Final/Assets/Hour 14 Package/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "Game";
+	public string sceneToLoad1 = "ahartman_FINAL";
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+		public void LoadScene ()
+	{
+		SceneManager.LoadScene(sceneToLoad1);
+	}
+}
